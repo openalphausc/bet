b15dbba06e09772029c6144aa36d1272e665c37a
Colin Spiridonov <spiridon@usc.edu>
2020-09-14 21:37:01 -0700

    Helen and I started working on initial sprites being added and the first monster sliding onto the screen

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+
+    public enum MonsterState
+    {
+        slidingOn,
+        center,
+        slidingOff,
+        despawned,
+    }
+
+    public MonsterState state = MonsterState.slidingOn;
+
+    public float speed = 10.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(-30, 0, 5);
+
+        Debug.Log("monster is spawned");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // slide in to the right
+        if(state == MonsterState.slidingOn) transform.position = new Vector3(transform.position.x + Time.deltaTime * speed, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public List<GameObject> monstersToSpawn;
+
+    private float spawnerTimer;
+
+    public GameObject birdPerson;
+    private bool birdPersonSpawned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monstersToSpawn = new List<GameObject>();
+        monstersToSpawn.Add(birdPerson);
+
+        spawnerTimer = 0;
+
+        birdPersonSpawned = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnerTimer += Time.deltaTime;
+
+        //Debug.Log("spawner's timer is " + spawnerTimer);
+
+        // spawn birdPerson after certain amount of time
+        if(spawnerTimer >= 1.0f && !birdPersonSpawned)
+        {
+            Instantiate(birdPerson, new Vector3(0, 0, 0), Quaternion.identity);
+            birdPersonSpawned = true;
+        }
+    }
+}
