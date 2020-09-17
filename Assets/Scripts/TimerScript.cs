234ba5550df62957bdf9f441c74c81c241759a14
Charlie Feuerborn <cfeuerbo@usc.edu>
2020-09-16 20:58:34 -0700

    Added timerscript as example code

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField] private TMP_Text uiText;
+
+    private float timer;
+    public List<float> trackSplits;
+    public bool runTimer;
+
+    void Start()
+    {
+        timer = 0;
+        runTimer = true;
+        trackSplits = new List<float>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (runTimer)
+        {
+            timer += Time.deltaTime;
+            uiText.text = timer.ToString("F");
+        }
+
+        else
+        {
+            addTime();
+            PersistentManagerScript.Instance.floorSplits.Add((timer));
+            timer = 0;
+            runTimer = true;
+        }
+        
+    }
+
+    public void addTime()
+    {
+        
+        trackSplits.Add(timer);
+    }}
