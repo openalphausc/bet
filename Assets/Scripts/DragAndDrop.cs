114e535ec666fe544d1eabf5705fea95baa7cafb
Philippe Cutillas <pcutillas@email.arizona.edu>
2020-09-19 15:11:54 -0700

    first commit, added ingredient sprites and made drag script

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+
+    private bool dragging = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        dragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dragging == true)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePos);
+        }
+    }
+}
