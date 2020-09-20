f23358d36586e4187dcc3bd5c7b1b16fab46d46e
Justin Sindecuse-Hayden <sindecus@usc.edu>
2020-09-19 21:12:23 -0700

    Drag Works, Added Temporary Sprites

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class DragAndDrop : MonoBehaviour
 {
 
+    private float startPosX;
+    private float startPosY;
     private bool dragging = false;
+    private Vector2 mousePos;
 
     // Start is called before the first frame update
     void Start()
@@ -26,10 +29,12 @@ public class DragAndDrop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        //Debug.Log(Camera.main.nearClipPlane);
         if (dragging == true)
         {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            transform.Translate(mousePos);
+            Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 30.0f)) - transform.position;
+            transform.Translate(point);
         }
     }
 }
