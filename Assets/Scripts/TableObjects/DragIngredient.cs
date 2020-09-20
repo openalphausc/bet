f7d3dfb3c6f9500f5cf25320ab0df64e721bdfbf
Justin Sindecuse-Hayden <sindecus@usc.edu>
2020-09-20 00:13:28 -0700

    Glass fills upon coming into contact with ingredient, ingredient disappears upon contact with glass, glass sticks to customer as they walk away.

---
+++ Assets/Scripts/TableObjects/DragIngredient.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragIngredient : MonoBehaviour
+{
+
+    public GameObject emptyGlass;
+    public GameObject fullGlass;
+    private bool instanceCreated = false;
+    private bool dragging = false;
+    private Vector2 mousePos;
+    private bool full = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+        dragging = true;
+        //Create an instance of the object to remain while you drap the ingredient
+        if (!instanceCreated)
+        {
+            Instantiate(gameObject);
+        }
+        instanceCreated = true;
+
+    }
+
+    public void OnMouseUp()
+    {
+        dragging = false;
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collisionInfo)
+    {
+        if (collisionInfo.gameObject.name == "EmptyGlass" && full == false)
+        {
+            full = true;
+            Instantiate(fullGlass);
+            Destroy(collisionInfo.gameObject);
+            Destroy(gameObject);
+        }
+        if (collisionInfo.gameObject.name == "FullGlass" && full == true)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //need to use ScreenToWorldPoint in order to put the mouse position into terms of scene position instead of screen pixel position
+        //to get vector from x to y:
+        //XY = y-pos - x-pos
+        if (dragging == true)
+        {
+            Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 30.0f)) - transform.position;
+            transform.Translate(point);
+        }
+    }
+}
---
+++ Assets/Scripts/TableObjects/GlassFill.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassFill : MonoBehaviour
+{
+    private bool full = false;
+    private ArrayList ingredients = new ArrayList();
+    private bool dragging = false;
+    private bool purchased = false;
+    private GameObject monsterCol;
+    public GameObject emptyGlass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+        dragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collisionInfo)
+    {
+        if(collisionInfo.gameObject.tag == "Ingredient")
+        {
+            full = true;
+            ingredients.Add(collisionInfo.gameObject.name);
+        }
+        if (collisionInfo.gameObject.tag == "Monster")
+        {
+            purchased = true;
+            monsterCol = collisionInfo.gameObject;
+            Instantiate(emptyGlass);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (purchased == true)
+        {
+            gameObject.transform.position = monsterCol.gameObject.transform.position;
+        }
+        else if (dragging == true)
+        {
+            Debug.Log("Here");
+            Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 30.0f)) - transform.position;
+            transform.Translate(point);
+        }
+    }
+}
