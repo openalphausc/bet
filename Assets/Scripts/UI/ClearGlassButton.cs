2ce6a106f86f2fae000ff927e0bbf33cc8f8c8c1
Sam Mehdi <smehdi@usc.edu>
2020-10-10 18:08:55 -0700

    Clear button now works even after the first glass

---
+++ Assets/Scripts/UI/ClearGlassButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearGlassButton : MonoBehaviour
+{
+    public void Clear()
+    {
+        GlassFill glass = FindObjectOfType<GlassFill>();
+        glass.clearIngredients();
+    }
+}
