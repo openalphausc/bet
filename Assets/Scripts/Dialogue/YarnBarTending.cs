300653ed0d2ff10b0062657972bc881e3f55d3b3
Justin Sindecuse-Hayden <sindecus@usc.edu>
2020-10-20 18:32:10 -0700

    Implemented Yarn Commands

---
+++ Assets/Scripts/Dialogue/YarnBarTending.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class YarnBarTending : MonoBehaviour
+{
+    [YarnCommand("stayAfter")]
+    public void stayAfter()
+    {
+        Debug.Log(dataStorage.stayingMonsters.Count);
+        dataStorage.stayingMonsters.Add(gameObject);
+        Debug.Log(dataStorage.stayingMonsters.Count);
+        //dataStorage.GetComponent("stayingMonsters") = stayingMonsters;
+    }
+
+    void Start()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameManagement/dataStorage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dataStorage : MonoBehaviour
+{
+    //List of monsters asked to stay afterHours
+    public static ArrayList stayingMonsters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stayingMonsters = new ArrayList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
