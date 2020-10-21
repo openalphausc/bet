5cf9349f55a2a8d535db1097261ef6780a18cc6d
Philippe Cutillas <pcutillas@email.arizona.edu>
2020-10-21 16:06:29 -0600

    Avoided bug

--- Assets/Scripts/Dialogue/YarnBarTending.cs
+++ Assets/Scripts/Dialogue/YarnBarTending.cs
@@ -1,21 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 using Yarn.Unity;
 
 public class YarnBarTending : MonoBehaviour
 {
-    [YarnCommand("stayAfter")]
-    public void stayAfter()
+
+    [YarnCommand("inviteToAfterHours")]
+    public void StayAfter()
     {
-        Debug.Log(dataStorage.stayingMonsters.Count);
-        dataStorage.stayingMonsters.Add(gameObject);
-        Debug.Log(dataStorage.stayingMonsters.Count);
-        //dataStorage.GetComponent("stayingMonsters") = stayingMonsters;
+        // The code below was to see the progression of the stayingMonsters list
+        //Debug.Log("Currently invited monsters:");
+        //foreach (GameObject monster in dataStorage.stayingMonsters)
+        //{
+        //    Debug.Log("\t" + monster);
+        //}
+
+        // Add the current monster to the stayingMonsters list
+        // NOTE: Adding a clone since the original gets despawned/destroyed
+
+        GameObject newInstance = Instantiate(gameObject);
+        newInstance.SetActive(false);
+
+        dataStorage.stayingMonsters.Add(newInstance);
+        Debug.Log(newInstance + " invited to after hours.");
     }
 
-    void Start()
-    {
-
-    }
 }
