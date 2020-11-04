5659c2e59d93bb64256ad9229b02603b81e76b7d
Colin Spiridonov <spiridon@usc.edu>
2020-11-04 10:32:27 -0800

    Added blender sprite

---
+++ Assets/Scripts/TableObjects/Blender.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blender : MonoBehaviour
+{
+    private GlassMove glassMove;
+
+    private bool blending;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        blending = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        glassMove = GameObject.FindWithTag("Glass").GetComponent<GlassMove>();
+    }
+
+    public void OnMouseUp()
+    {
+        if (glassMove.holding)
+        {
+            glassMove.holding = false;
+            // disappear the glass
+            blending = true;
+            Debug.Log("Start blending!");
+        }
+    }
+}
