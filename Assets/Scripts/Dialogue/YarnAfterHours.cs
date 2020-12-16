95d47068fe1211d676b50013690db6f9b85dc9c0
Sam Mehdi <smehdi@usc.edu>
2020-12-16 14:43:34 -0800

    Added AfterHours decisions functions for updating points

--- Assets/Scripts/Dialogue/YarnAfterHours.cs
+++ Assets/Scripts/Dialogue/YarnAfterHours.cs
@@ -23,4 +23,52 @@ public class YarnAfterHours : MonoBehaviour
         SceneManager.LoadScene("Tabsheet");
     }
 
+    // Update monster points for right answer
+    [YarnCommand("rightAnswer")]
+    public void updatePointsRight()
+    {
+        string talkingMonster = dataStorage.stayingMonster;
+        // find the talking monster in the points array
+        NameComp nameComp = new NameComp();
+        int index = dataStorage.monsters.BinarySearch(AfterHoursMonsterSpawner.currentMonster.GetComponent<Monster>(), nameComp);
+        // if the monster is in the data storage array, update its points
+        if (index >= 0)
+        {
+            dataStorage.monsters[index].pointsEarned += 50;
+            dataStorage.monsters[index].totalPoints += 50;
+        }
+        else
+        {
+            Debug.Log("Unknown monster in After Hours, not in points array");
+        }
+    }
+
+    // Update monster points for right answer
+    [YarnCommand("wrongAnswer")]
+    public void updatePointsWrong()
+    {
+        string talkingMonster = dataStorage.stayingMonster;
+        // find the talking monster in the points array
+        NameComp nameComp = new NameComp();
+        int index = dataStorage.monsters.BinarySearch(AfterHoursMonsterSpawner.currentMonster.GetComponent<Monster>(), nameComp);
+        // if the monster is in the data storage array, update its points
+        if (index >= 0)
+        {
+            dataStorage.monsters[index].pointsEarned += 25;
+            dataStorage.monsters[index].totalPoints += 50;
+        }
+        else
+        {
+            Debug.Log("Unknown monster in After Hours, not in points array");
+        }
+    }
+
+    // name comparison class, don't mind
+    public class NameComp : IComparer<Monster>
+    {
+        public int Compare(Monster x, Monster y)
+        {
+            return x.name.CompareTo(y.name);
+        }
+    }
 }
