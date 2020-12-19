383257d43588ddfb972de16518e3b3b1257ae3e2
Coco Zhang <cmzhang@usc.edu>
2020-12-18 23:12:08 -0500

    ending scene assets

---
+++ Assets/Scripts/Monsters/EndingScene.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < MonsterSpawner.MonstersToSpawn.Count; ++i)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
